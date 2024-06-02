d46c18fb832ce72e22bce9e0af4a8abd2f23be1b
Lone G <gyllone1993@gmail.com>
2024-06-02 19:24:12 +0800

    fix

--- programs/game-program/src/lib.rs
+++ programs/game-program/src/lib.rs
@@ -6,7 +6,7 @@ use anchor_lang::prelude::*;
 
 use instructions::*;
 
-declare_id!("C7nbA2iATFvC74iN4gVxdWBA2YT3eVg13ps2PBJXMTwh");
+declare_id!("rP9PDkd2kfGARTjDPHtVMr45WbY8bBrFqLfyt9RrVF1");
 
 #[program]
 pub mod game_program {
